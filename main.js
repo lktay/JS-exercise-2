6dbca4687c2c8c7003c26e1f3b8f0bc413f36018
lktay <leotay0807@gmail.com>
2022-12-10 17:27:11 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,99 @@
+// (function helloWorld() {
+// console.log("Hello, World!")
+// })();
+
+
+//exercise 1
+function helloWorld() {
+return console.log("Hello, World!")
+}
+
+console.log(helloWorld());
+
+//exercise 2
+function quadrupler(num) {
+
+  return num * 4;
+}
+console.log(quadrupler(5));
+
+//exercise 3
+
+function welcome(firstName, lastName) {
+  return "Hello " + firstName + " " + lastName + ", how can I help you?";
+}
+
+let firstName = "DAVE";
+let lastName = "KAY";
+
+console.log(welcome(firstName, lastName.toLowerCase()));
+
+//exercise 4
+function temperatureConverter(f) {
+  return c = (f - 32) * (5/9) 
+}
+temperatureConverter(70);
+console.log(c);
+temperatureConverter(80);
+console.log(c);
+temperatureConverter(90);
+console.log(Math.floor(c));
+
+//exercise 5
+console.log("Exercise 5")
+let name = Leoni
+let age = 26
+
+function dogAge(age) {
+
+let dogYears;
+
+if (age <= 15 ) {
+  dogYears = age / 15;
+} 
+
+else if (age > 15 && age < 24 ) {
+  dogYears = (age - 15) / (9 + 1); 
+}
+
+else {
+  dogYears = (age - 24) / (5 + 2); 
+}
+return dogYears.toFixed(1);
+}
+
+
+
+
+
+
+
+
+
+
+  // if (age >= 1)  
+//  {
+  // dogYears == 15;
+  // age = age - 1;}
+// 
+  // else if (age >= 2)
+  // {
+  // dogYears += 9;
+// age = age - 1;}
+// 
+  // else if (age >= 3)
+  // {
+  // return dogYears = age * 5;}
+// }
+// dogAge();
+// console.log(dogYears);
+
+
+
+//exercise 6
+
+function calculator(num1, num2, operator) {
+
+
+
+}
